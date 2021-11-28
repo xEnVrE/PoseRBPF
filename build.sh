8a73d54bedd77d614d43bae20c6096c9a457088e
Nicola Piga <nicolapiga@gmail.com>
2021-11-28 11:31:35 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 cd ./utils/RoIAlign;
 sh build.sh;
-cd ../sdf_layer/;
+cd -
+cd ./utils/sdf_layer/;
 sh build.sh;
-cd ../posecnn_layer/;
+cd -
+cd ./utils/posecnn_layer/;
 python setup.py build develop;
-cd ../../utils;
+cd -
+cd utils;
 python setup.py build_ext --inplace
-
+cd -
