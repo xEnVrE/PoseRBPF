96af74e20a65aeb531454189059b8759fbd5bee8
Nicola Piga <nicolapiga@gmail.com>
2021-11-28 11:36:51 +0100

    Add scripts for automated ycbv evaluation

---
+++ run.sh
@@ -0,0 +1,79 @@
+bash scripts/test_ycb_rgbd/val_ycb_010_rgbd.sh 0 1
+bash scripts/test_ycb_rgbd/val_ycb_010_rgbd.sh 0 2
+bash scripts/test_ycb_rgbd/val_ycb_010_rgbd.sh 0 3
+
+bash scripts/test_ycb_rgbd/val_ycb_002_rgbd.sh 0 1
+bash scripts/test_ycb_rgbd/val_ycb_002_rgbd.sh 0 2
+bash scripts/test_ycb_rgbd/val_ycb_002_rgbd.sh 0 3
+bash scripts/test_ycb_rgbd/val_ycb_002_rgbd.sh 0 4
+
+bash scripts/test_ycb_rgbd/val_ycb_003_rgbd.sh 0 1
+bash scripts/test_ycb_rgbd/val_ycb_003_rgbd.sh 0 2
+bash scripts/test_ycb_rgbd/val_ycb_003_rgbd.sh 0 3
+
+bash scripts/test_ycb_rgbd/val_ycb_004_rgbd.sh 0 1
+bash scripts/test_ycb_rgbd/val_ycb_004_rgbd.sh 0 2
+bash scripts/test_ycb_rgbd/val_ycb_004_rgbd.sh 0 3
+bash scripts/test_ycb_rgbd/val_ycb_004_rgbd.sh 0 4
+bash scripts/test_ycb_rgbd/val_ycb_004_rgbd.sh 0 5
+
+bash scripts/test_ycb_rgbd/val_ycb_005_rgbd.sh 0 1
+bash scripts/test_ycb_rgbd/val_ycb_005_rgbd.sh 0 2
+bash scripts/test_ycb_rgbd/val_ycb_005_rgbd.sh 0 3
+bash scripts/test_ycb_rgbd/val_ycb_005_rgbd.sh 0 4
+bash scripts/test_ycb_rgbd/val_ycb_005_rgbd.sh 0 5
+bash scripts/test_ycb_rgbd/val_ycb_005_rgbd.sh 0 7
+
+bash scripts/test_ycb_rgbd/val_ycb_006_rgbd.sh 0 1
+bash scripts/test_ycb_rgbd/val_ycb_006_rgbd.sh 0 2
+
+bash scripts/test_ycb_rgbd/val_ycb_007_rgbd.sh 0 1
+bash scripts/test_ycb_rgbd/val_ycb_007_rgbd.sh 0 2
+bash scripts/test_ycb_rgbd/val_ycb_007_rgbd.sh 0 3
+bash scripts/test_ycb_rgbd/val_ycb_007_rgbd.sh 0 4
+
+bash scripts/test_ycb_rgbd/val_ycb_008_rgbd.sh 0 1
+
+bash scripts/test_ycb_rgbd/val_ycb_009_rgbd.sh 0 1
+
+# bash scripts/test_ycb_rgbd/val_ycb_010_rgbd.sh 0 1
+# bash scripts/test_ycb_rgbd/val_ycb_010_rgbd.sh 0 2
+# bash scripts/test_ycb_rgbd/val_ycb_010_rgbd.sh 0 3
+
+bash scripts/test_ycb_rgbd/val_ycb_011_rgbd.sh 0 1
+bash scripts/test_ycb_rgbd/val_ycb_011_rgbd.sh 0 2
+
+bash scripts/test_ycb_rgbd/val_ycb_019_rgbd.sh 0 1
+bash scripts/test_ycb_rgbd/val_ycb_019_rgbd.sh 0 2
+bash scripts/test_ycb_rgbd/val_ycb_019_rgbd.sh 0 3
+
+bash scripts/test_ycb_rgbd/val_ycb_021_rgbd.sh 0 1
+bash scripts/test_ycb_rgbd/val_ycb_021_rgbd.sh 0 2
+bash scripts/test_ycb_rgbd/val_ycb_021_rgbd.sh 0 3
+bash scripts/test_ycb_rgbd/val_ycb_021_rgbd.sh 0 4
+
+bash scripts/test_ycb_rgbd/val_ycb_024_rgbd.sh 0 1
+bash scripts/test_ycb_rgbd/val_ycb_024_rgbd.sh 0 2
+
+bash scripts/test_ycb_rgbd/val_ycb_025_rgbd.sh 0 1
+bash scripts/test_ycb_rgbd/val_ycb_025_rgbd.sh 0 2
+
+bash scripts/test_ycb_rgbd/val_ycb_035_rgbd.sh 0 1
+bash scripts/test_ycb_rgbd/val_ycb_035_rgbd.sh 0 2
+bash scripts/test_ycb_rgbd/val_ycb_035_rgbd.sh 0 3
+bash scripts/test_ycb_rgbd/val_ycb_035_rgbd.sh 0 4
+
+bash scripts/test_ycb_rgbd/val_ycb_036_rgbd.sh 0 1
+
+bash scripts/test_ycb_rgbd/val_ycb_037_rgbd.sh 0 1
+
+bash scripts/test_ycb_rgbd/val_ycb_040_rgbd.sh 0 1
+bash scripts/test_ycb_rgbd/val_ycb_040_rgbd.sh 0 2
+
+bash scripts/test_ycb_rgbd/val_ycb_051_rgbd.sh 0 1
+bash scripts/test_ycb_rgbd/val_ycb_051_rgbd.sh 0 2
+
+bash scripts/test_ycb_rgbd/val_ycb_052_rgbd.sh 0 1
+bash scripts/test_ycb_rgbd/val_ycb_052_rgbd.sh 0 2
+
+bash scripts/test_ycb_rgbd/val_ycb_061_rgbd.sh 0 1
